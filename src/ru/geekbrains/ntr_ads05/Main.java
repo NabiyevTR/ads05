90c3f09a64eca5bada07f7ba8ef828eab8f5e0eb
NabiyevTR <NabiyeTR@gmail.com>
2021-02-16 10:08:56 +0300

    Task2

--- src/ru/geekbrains/ntr_ads05/Main.java
+++ src/ru/geekbrains/ntr_ads05/Main.java
@@ -8,9 +8,36 @@ public class Main {
 
         //Задание 1. Возведение в степень
         powAndDisplay(2, 2);
-        powAndDisplay(-2,4);
-        powAndDisplay(-2.5,0);
-        powAndDisplay(2,-2);
+        powAndDisplay(-2, 4);
+        powAndDisplay(-2.5, 0);
+        powAndDisplay(2, -2);
+        System.out.println("--------------------");
+
+        //Задание 2. Задача о рюкзаке.
+        Box box = new Box(
+                new Book("Паттерны проектирования", 2f, 1200f),
+                new Book("Война и мир", 5f, 700f),
+                new Book("Spring в действии"),
+                new Book("Spring в действии"),
+                new Book("Spring в действии"),
+                new Book("Spring в действии"),
+                new Book("MVC .NET"),
+                new Book("Преступление и наказание"),
+                new DifferentStuff("Телефон", 0.4f, 35000f),
+                new DifferentStuff("Планшет", 0.9f, 45000f)
+        );
+        box.display();
+        System.out.println("--------------------");
+
+        box.setMaxWeight(10f);
+        box.addItem(
+                new NoteBook("HP8570w")
+        );
+        box.display();
+        System.out.println("--------------------");
+
+        box.setMaxWeight(3f);
+        box.display();
 
     }
 }
